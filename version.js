5dfd8ca05a5ada854158b31ab5a3655c4db51359
pv15suporte-gif <pv15suporte@gmail.com>
2026-07-17 14:43:44 -0300

    Update version.js

--- version.js
+++ version.js
@@ -1,6 +1,6 @@
 {
     "version": "1.0.0",
-    "download_url": "https://github.com/pv15suporte-gif/SCA/",
+    "download_url": "https://github.com/pv15suporte-gif/SCA/releases/download/1.0.0/SCADP_Setup_v1.0.0.exe",
     "notes": [
         "Release de testes",
         "Adicionado configurador banco de dados"
