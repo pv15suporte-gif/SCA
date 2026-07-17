0c24ca652343176e2596ba83e07bc44c00f74499
pv15suporte-gif <pv15suporte@gmail.com>
2026-07-17 17:36:47 -0300

    Update version.js

--- version.js
+++ version.js
@@ -1,6 +1,6 @@
 {
     "version": "2.0.0",
-    "download_url": "https://github.com/pv15suporte-gif/SCA/releases/download/1.0.0/SCADP_Setup_v2.0.0.exe",
+    "download_url": "https://github.com/pv15suporte-gif/SCA/releases/download/2.0.0/SCADP_Setup_v2.0.0.exe",
     "notes": [
         "Correção de Bugs",
         "Corrigido cadastro de colaborador"
