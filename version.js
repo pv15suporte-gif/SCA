162eb6e49548f5c35212a6703c8465dd4d8863b4
pv15suporte-gif <pv15suporte@gmail.com>
2026-07-20 17:56:27 -0300

    Bump version to 2.0.5 and update download URL
    Updated version and download URL to 2.0.5. Added new notes for bug fixes and features.

--- version.js
+++ version.js
@@ -1,9 +1,10 @@
 {
-    "version": "2.0.0",
-    "download_url": "https://github.com/pv15suporte-gif/SCA/releases/download/2.0.0/SCADP_Setup_v2.0.0.exe",
+    "version": "2.0.5",
+    "download_url": "https://github.com/pv15suporte-gif/SCA/releases/download/2.0.5/SCADP_Setup_v2.0.5.exe",
     "notes": [
         "Correção de Bugs",
-        "Corrigido cadastro de colaborador"
+        "Corrigido cadastro de exames",
+        "Adicionado exclusão logica para perfil DP"
     ],
     "mandatory": false
 }
